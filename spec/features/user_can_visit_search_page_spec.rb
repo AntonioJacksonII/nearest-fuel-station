fe1d019e58440ef2e6db7bdaac33e10fbcff2122
Antonio Jackson <antoniojackson@gmail.com>
2020-07-27 10:50:51 -0600

    Updating tests to ensure css elements are not empty

--- spec/features/user_can_visit_search_page_spec.rb
+++ spec/features/user_can_visit_search_page_spec.rb
@@ -8,12 +8,25 @@ describe "user can visit the search page" do
     expect(current_path).to eq(search_path)
 
     expect(page).to have_css('#name')
+
     expect(page).to have_css('#address')
+      address = find('#address').text
+      expect(address).to_not be_empty
     expect(page).to have_css('#fuel_type')
+      fuel_type = find('#fuel_type').text
+      expect(fuel_type).to_not be_empty
     expect(page).to have_css('#access_times')
+      access_times = find('#access_times').text
+      expect(access_times).to_not be_empty
     expect(page).to have_css('#distance')
+      distance = find('#distance').text
+      expect(distance).to_not be_empty
     expect(page).to have_css('#travel_time')
+      travel_time = find('#travel_time').text
+      expect(travel_time).to_not be_empty
     expect(page).to have_css('#directions')
+      directions = find('#directions').text
+      expect(directions).to_not be_empty
   end
 end
 
